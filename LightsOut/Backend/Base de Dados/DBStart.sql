b4d6ce8f8bd557da2ead89d48488a6eda647bd04
António Santos <antoniomsantos99@gmail.com>
2021-05-15 21:39:06 +0100

    Inicio ligação base de dados

--- LightsOut/Backend/Base de Dados/DBStart.sql
+++ LightsOut/Backend/Base de Dados/DBStart.sql
@@ -20,19 +20,19 @@ CREATE TABLE [Prova] (
   [data] datetime,
   [horaProva] time,
   [idNotificacao] int,
-  [idLocalizacao] int
+  [idLocalizacao] nvarchar(255)
 )
 GO
 
 CREATE TABLE [Epoca] (
-  [id] int PRIMARY KEY IDENTITY(1, 1),
-  [ano] int
+  [ano] int PRIMARY KEY
 )
 GO
 
 CREATE TABLE [Localizacao] (
-  [id] int PRIMARY KEY IDENTITY(1, 1),
-  [nacionalidade] int,
+  [id] nvarchar(255) PRIMARY KEY,
+  [nome] nvarchar(255),
+  [nacionalidade] nvarchar(255),
   [latitude] float,
   [longitude] float
 )
@@ -40,7 +40,7 @@ GO
 
 CREATE TABLE [Qualificacao] (
   [idProva] int PRIMARY KEY,
-  [idPiloto] int,
+  [idPiloto] nvarchar(255),
   [posicaoQualificacao] int,
   [qualificao1] time,
   [qualificao2] time,
@@ -50,7 +50,7 @@ GO
 
 CREATE TABLE [Resultado] (
   [idProva] int PRIMARY KEY,
-  [idPiloto] int,
+  [idPiloto] nvarchar(255),
   [posicaoFinal] int,
   [posicaoInicial] int,
   [tempo] time,
@@ -60,11 +60,11 @@ CREATE TABLE [Resultado] (
 GO
 
 CREATE TABLE [Piloto] (
-  [id] int PRIMARY KEY IDENTITY(1, 1),
+  [id] nvarchar(255) PRIMARY KEY,
   [nome] nvarchar(255),
   [numero] int,
   [dataNascimento] date,
-  [nacionalidade] int,
+  [nacionalidade] nvarchar(255),
   [equipaId] int
 )
 GO
@@ -72,21 +72,21 @@ GO
 CREATE TABLE [Equipa] (
   [id] int PRIMARY KEY IDENTITY(1, 1),
   [nome] nvarchar(255),
-  [nacionalidade] int,
+  [nacionalidade] nvarchar(255),
   [idEpoca] int
 )
 GO
 
 CREATE TABLE [Pais] (
-  [id] int PRIMARY KEY IDENTITY(1, 1),
-  [nome] nvarchar(255)
+  [nome] nvarchar(255) PRIMARY KEY,
+  [nacionalidade] nvarchar(255)
 )
 GO
 
 ALTER TABLE [Notificacao] ADD FOREIGN KEY ([idUtilizador]) REFERENCES [Utilizador] ([id])
 GO
 
-ALTER TABLE [Prova] ADD FOREIGN KEY ([idEpoca]) REFERENCES [Epoca] ([id])
+ALTER TABLE [Prova] ADD FOREIGN KEY ([idEpoca]) REFERENCES [Epoca] ([ano])
 GO
 
 ALTER TABLE [Prova] ADD FOREIGN KEY ([idNotificacao]) REFERENCES [Notificacao] ([id])
@@ -95,7 +95,7 @@ GO
 ALTER TABLE [Prova] ADD FOREIGN KEY ([idLocalizacao]) REFERENCES [Localizacao] ([id])
 GO
 
-ALTER TABLE [Localizacao] ADD FOREIGN KEY ([nacionalidade]) REFERENCES [Pais] ([id])
+ALTER TABLE [Localizacao] ADD FOREIGN KEY ([nacionalidade]) REFERENCES [Pais] ([nome])
 GO
 
 ALTER TABLE [Qualificacao] ADD FOREIGN KEY ([idProva]) REFERENCES [Prova] ([id])
@@ -110,14 +110,14 @@ GO
 ALTER TABLE [Resultado] ADD FOREIGN KEY ([idPiloto]) REFERENCES [Piloto] ([id])
 GO
 
-ALTER TABLE [Piloto] ADD FOREIGN KEY ([nacionalidade]) REFERENCES [Pais] ([id])
+ALTER TABLE [Piloto] ADD FOREIGN KEY ([nacionalidade]) REFERENCES [Pais] ([nome])
 GO
 
 ALTER TABLE [Piloto] ADD FOREIGN KEY ([equipaId]) REFERENCES [Equipa] ([id])
 GO
 
-ALTER TABLE [Equipa] ADD FOREIGN KEY ([nacionalidade]) REFERENCES [Pais] ([id])
+ALTER TABLE [Equipa] ADD FOREIGN KEY ([nacionalidade]) REFERENCES [Pais] ([nome])
 GO
 
-ALTER TABLE [Equipa] ADD FOREIGN KEY ([idEpoca]) REFERENCES [Epoca] ([id])
+ALTER TABLE [Equipa] ADD FOREIGN KEY ([idEpoca]) REFERENCES [Epoca] ([ano])
 GO
---
+++ LightsOut/Backend/DataManager/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Dapper;
+
+using Newtonsoft.Json.Linq;
+
+namespace LightsOut
+{
+    public class DataManager
+    {
+
+        public string conString =
+            "Data Source=(local);Initial Catalog=LightsOut;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
+        void loadEpocas(int from, int to)
+        {
+            /*Conecta á base de dados*/
+            using (var connection = new SqlConnection(conString))
+            {
+                connection.Open();
+                /*Adiciona cada país individualmente á base de dados TODO: Passar para um bulkinsert do dapper PLUS*/
+                for (int epoca = from; epoca <= to; epoca++)
+                {
+                    var sql = String.Format("If Not Exists(select * from [LightsOut].[dbo].Epoca where ano=\'{0}\') begin insert into [LightsOut].[dbo].Epoca values ({0}) end", epoca);
+                    connection.Query(sql);
+                }
+            }
+            Console.WriteLine("Epocas Adicionadas!");
+        }
+        
+        async void loadCircuitos()
+        {
+            /*Conecta á base de dados*/
+            using (var connection = new SqlConnection(conString))
+            {
+                connection.Open();
+                
+                /*Vê as epocas presentes na base de dados*/
+                var result = connection.Query<int>("select ano from [LightsOut].[dbo].Epoca").ToList();
+
+                foreach (var epoca in result)
+                {
+                    JObject info = getJsonFromURL(String.Format("http://ergast.com/api/f1/{0}/circuits.json",epoca));
+                    foreach (var circuito in info["MRData"]["CircuitTable"]["Circuits"].ToList())
+                    {
+                        var sql = String.Format(
+                            "If Not Exists(select * from [LightsOut].[dbo].Localizacao where id=\'{0}\') Begin insert into [LightsOut].[dbo].Localizacao values (\'{0}\',\'{1}\',\'{2}\',{3},{4}) End",
+                            circuito["circuitId"],circuito["circuitName"],circuito["Location"]["country"] ,circuito["Location"]["lat"],circuito["Location"]["long"]);
+                        var res = connection.Query(sql);
+                        
+                    }
+                }
+                
+            }
+        }
+
+        /* Carrega lista de Países para a base de dados */
+        void loadCountries()
+        {
+            /* Cria uma lista de tuplos com todos os países tal como a sua respetiva nacionalidade*/
+            List<Tuple<String, String>> countries = LoadJson("D:\\DEV\\GitHub projects\\LI4-20-21\\LightsOut\\Backend\\DataManager\\Countries.json")
+                .Select(x => new Tuple<String, String>(x["en_short_name"], x["nationality"]))
+                    .ToList();
+
+            /*Conecta á base de dados*/
+            using (var connection = new SqlConnection(conString))
+            {
+                connection.Open();
+                /*Adiciona cada país individualmente á base de dados TODO: Passar para um bulkinsert do dapper PLUS*/
+                foreach (var country in countries)
+                {
+                    var sql = String.Format("If Not Exists(select * from [LightsOut].[dbo].Pais where nome=\'{0}\') begin insert into [LightsOut].[dbo].Pais values (\'{0}\',\'{1}\') end",
+                        country.Item1.Replace("'", "´"), country.Item2);
+                    connection.Query(sql);
+                }
+            }
+        }
+
+        public List<Dictionary<String,String>> LoadJson(String path)
+        {
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<Dictionary<String,String>>>(json);
+            }
+        }
+
+        JObject getJsonFromURL(String url)
+        {
+            using (var w = new WebClient())
+            {
+                try
+                {
+                    return (JObject) JsonConvert.DeserializeObject(w.DownloadString(url));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error downloading info!");
+                    return null;
+                }
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            DataManager dm = new DataManager();
+            dm.loadCountries();
+            dm.loadEpocas(2010,2020);
+            dm.loadCircuitos();
+        }
+    }
+}
